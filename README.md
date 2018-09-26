aa6d66cb83d8b21c89317f88ce3375449866578a
ousututu <xiawenjuan@ousututudeMacBook-Pro.local>
2018-09-26 18:58:08 +0800

    init

---
+++ README.md
@@ -0,0 +1,162 @@
+# vFetch
+
+## 安装
+
+```bash
+npm install vfetch
+```
+
+## 使用
+
+```javascript
+import VFetch from 'vfetch'
+
+const http = VFetch(opt)
+```
+
+## 初始化配置
+```javascript
+const httpConfig = {
+  conf: {
+    credentials: 'include',
+    baseUrl: '/api',
+    headers: {
+      Accept: 'application/json',
+      // 'Content-Type': 'application/json',
+    },
+  },
+  before: [
+    ([url, opt]) => {
+      console.log('hook1', url, opt)
+    },
+    ([url, opt]) => {
+      console.log('hook2', url, opt)
+    },
+  ],
+  after: [
+    (rsp) => {
+      console.log('after hook1', rsp)
+    },
+  ],
+  timeout: 5000,
+}
+```
+
+| 配置 | 说明 | 默认值 | 其它 |
+| - | - | - | - |
+| allow | client 允许的请求类型 | ['get', 'post', 'put', 'delete', 'option'] | 可选 |
+| before | 前置钩子(钩子数组) | []| 可选 | 
+| after | 后置钩子(钩子数组) | [] | 可选 | 
+| conf | fetch配置项 | | 参考fetch api 文档 |
+| error | 全局错误处理函数 | null | 可选 |
+
+## 方法
+
++ get(url, [parmas], [options]) // 基于初始化配置的 allow
++ post(url, [parmas], [options])  // 基于初始化配置的 allow
++ put(url, [parmas], [options]) // 基于初始化配置的 allow
++ delete(url, [parmas], [options]) // 基于初始化配置的 allow
++ option(url, [parmas], [options]) // 基于初始化配置的 allow
++ setErrorHook(func) 设置全局错误处理器
++ injectBefore(func) 增加前置拦截器
++ injectAfter(func) 增加后置拦截器
+
+## 内置error code
+
++ HTTP_STATUS_ERROR - 服务器未正常响应
++ REQUEST_TIMEOUT - 请求超时
++ TOKEN_EXPIRE - token校验失败
++ RESPONSE_PARSING_FAILED - reponse 解析出错
+
+## injectAfter function
+
+如果injectAfter function有返回值且返回值是HttpError的实例的话，当前请求的promise会被reject，
+这次请求的失败信息将也会触发onError事件。
+
+### 例子
+
+```javascript
+http.injectAfter(async function(response){
+  const rsp = await response.json()
+  // do some response check
+
+  return new vFetch.HttpError({
+    code: '001',
+    message: 'error test',
+    httpStatus: null,
+  })
+})
+```
+
+## Content-Type
+
+为了方便使用，下面这几种情况下vFetch会根据content-type去自动设置request body的类型，
+处理body对象的构建
+
++ application/x-www-form-urlencoded (默认) 如果传入的params是object类型，则自动构建请求body
++ multipart/form-data 自动根据请求的parmas object 构建 FromData对象
++ application/json 如果传入的params是object类型，则自动转换成JsonString，如果为string类型，则直接用传入的params
+
+请求的params为FormData类型时，request的body不会进行自动转换
+
+## httpError 
+
+### 构造函数
+
+httpError实例的构造函数为 vFetch.HttpError
+
+### 示例
+
+```javascript
+{
+  code: "TOKEN_EXPIRE"
+  httpStatus: 401
+  message: "用户认证失败"
+}
+```
+
+## 示例
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <script src="../dist/vFetch.js"></script>
+</head>
+<body>
+  <script>
+    console.log(window, VFetch.HttpError, VFetch.HTTP_ERROR_MAP, VFetch.httpConfig)
+    const http = VFetch(VFetch.httpConfig)
+
+    http.injectAfter(function(rst){
+      console.log('injectAfter', rst)
+      return new vFetch.HttpError({
+        code: '001',
+        message: 'error test',
+        httpStatus: null,
+      })
+    })
+    http.injectAfter(function(){
+      console.log(222);
+    })
+    http.setErrorHook(function(e){
+      console.log(e, 'error');
+    })
+
+    http.get('t.json', {a:2, c:3})
+      .then(rst => {
+        //console.log(rst, 'success');
+      }).catch(e => {
+        //console.log(e, 'error');
+      })
+  </script> 
+</body>
+</html>
+```
+
+## TODO
+**上传文件时从其他的```content-type```改为```multipart/form-data```时，```body```中的参数不携带```boundary```的问题**
---
+++ src/vFetch.js
@@ -0,0 +1,290 @@
+class HttpError {
+  constructor(errorInfo) {
+    const {
+      message,
+      code,
+      httpStatus = 200,
+    } = errorInfo
+    this.httpStatus = httpStatus
+    this.message = message
+    this.code = code
+
+    this.prototype = Object.create(Error.prototype)
+    this.prototype.constructor = this
+  }
+}
+
+const HTTP_ERROR_MAP = {
+  'HTTP_STATUS_ERROR': '服务器未正常响应',
+  'REQUEST_TIMEOUT': '请求超时',
+  'TOKEN_EXPIRE': 'token校验失效',
+  'RESPONSE_PARSING_FAILED': 'reponse解析出错',
+}
+
+HttpError.ERROR_CODE = {
+  HTTP_STATUS_ERROR: 'HTTP_STATUS_ERROR',
+  REQUEST_TIMEOUT: 'REQUEST_TIMEOUT',
+  TOKEN_EXPIRE: 'TOKEN_EXPIRE',
+  RESPONSE_PARSING_FAILED: 'RESPONSE_PARSING_FAILED',
+}
+
+class HttpShell {
+  constructor(option) {
+    const {
+      conf: config = { // request配置相关
+        baseUrl: '',
+        headers: {},
+      },
+      before = [], // 前置钩子
+      after = [], // 后置钩子
+      error = null, // error钩子
+      timeout = 5000,
+    } = option
+    this.config = config
+    this.beforeHooks = before
+    this.afterHooks = after
+    this.errorHook = error
+    this.timeout = timeout
+  }
+
+  _getQueryData(query, type = 'string') {
+    if (!query) {
+      return null
+    }
+    const queryList = []
+    const formData = new FormData()
+    Object.entries(query).forEach((q) => {
+      const [key, val] = q
+      if (val.length !== 0 && val) {
+        queryList.push(`${key}=${encodeURIComponent(val)}`)
+        formData.append(key, val)
+      }
+    })
+    return type === 'string' ? queryList.join('&') : formData
+  }
+
+  _getRequestOptions({ opt, method, params }) {
+    const finalOpt = {
+      method,
+      ...opt,
+    }
+    const headers = Object.assign({}, this.config.headers, opt.headers)
+    finalOpt.headers = headers
+    if (Object.prototype.toString.call(params) === '[object FormData]') {
+      finalOpt.body = params
+      return finalOpt
+    }
+    if (method !== 'GET' &&
+      method !== 'OPTION' &&
+      params) {
+      if (!finalOpt.headers['Content-Type']) {
+        finalOpt.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+      }
+      const contentType = finalOpt.headers['Content-Type']
+      if (contentType.indexOf('application/json') > -1) {
+        finalOpt.body = typeof params === 'string' ? params : JSON.stringify(params)
+      } else if (contentType.indexOf('application/x-www-form-urlencoded') > -1) {
+        finalOpt.body = Object.keys(params).map((key) => {
+          return `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+        })
+          .join('&')
+      } else if (contentType.indexOf('multipart/form-data') > -1) {
+        finalOpt.body = this._getQueryData(params, 'formData')
+      }
+    }
+    return Object.assign({}, this.config, finalOpt)
+  }
+
+  _checkResponse() {
+    return true
+  }
+
+  _initUrl(url, method, opt, params) {
+    const urlType = url.indexOf('://') !== -1 ? 'FULL' : 'PATH'
+
+    let queryString = null
+
+    let baseUrl = this.config.baseUrl || ''
+    if (opt && opt.baseUrl) {
+      const { baseUrl: bUrl } = baseUrl
+      baseUrl = bUrl
+    }
+    let finalUrl = urlType !== 'FULL' ?
+      baseUrl + url :
+      url
+
+    if (method === 'GET' || method === 'OPTION') {
+      queryString = this._getQueryData(params)
+      if (queryString && queryString.length) {
+        finalUrl += `?${queryString}`
+      }
+    }
+
+    return finalUrl
+  }
+
+  _sendRequestWithTimeOut(apiPromise, overHandler) {
+    return Promise.race([
+      apiPromise,
+      new Promise((_, reject) => { // eslint-disable-line
+        setTimeout(() => {
+          const error = new HttpError({
+            message: '请求超时',
+            code: HttpError.ERROR_CODE.REQUEST_TIMEOUT,
+            httpStatus: 901,
+          })
+          reject(error)
+          overHandler(error)
+        }, this.timeout)
+      }),
+    ])
+  }
+
+  _getApiPromise(http, finalUrl, finalOpt, overHandler, getOverStatus, setOver) {
+    return new Promise((resolve, reject) =>
+      http(finalUrl, finalOpt)
+        .then((rsp) => {
+          if (this._checkResponse(rsp, reject)) {
+            return rsp.json()
+          }
+          return {}
+        })
+        .catch(() => {
+          const error = new HttpError({
+            message: '请求失败，请检查网络情况，并联系管理员。',
+            code: HttpError.ERROR_CODE.RESPONSE_PARSING_FAILED,
+            httpStatus: null,
+          })
+          reject(error)
+          overHandler(error)
+        })
+        .then((rsp) => {
+          this.afterHooks.forEach(hook => {
+            if (!getOverStatus()) {
+              const hookRst = hook(rsp)
+              if (hookRst instanceof HttpError) {
+                reject(hookRst)
+                overHandler(hookRst)
+              }
+            }
+          })
+          setOver()
+          resolve(rsp)
+        }))
+  }
+
+
+  _sendRequest(http, url, method = 'GET', params = {}, opt = {}) {
+
+    const fetchUrl = this._initUrl(url, method, opt, params)
+
+    const fetchOpt = this._getRequestOptions({
+      opt,
+      method,
+      params,
+    })
+
+    const [finalUrl, finalOpt] = this.beforeHooks.reduce(([url, opt], hook) => {
+      return hook([url, opt]) || [url, opt]
+    }, [fetchUrl, fetchOpt])
+    let isOver = false
+    const overHandler = (error) => {
+      !isOver && this.errorHook(error, fetchUrl)
+      isOver = true
+    }
+    const apiPromise = this._getApiPromise(http, finalUrl, finalOpt, overHandler, () => isOver, () => { isOver = true })
+    const request = this._sendRequestWithTimeOut(apiPromise, overHandler)
+    return request
+  }
+
+  injectAfter(after) {
+    console.log(this)
+    after && this.afterHooks.push(after)
+  }
+
+  injectBefore(before) {
+    before && this.beforeHooks.push(before)
+  }
+
+  setError(onError) {
+    if (onError) {
+      this.errorHook = onError
+    }
+  }
+
+  get(http, url, params, opt) {
+    return this._sendRequest(http, url, 'GET', params, opt)
+  }
+
+  post(http, url, params, opt) {
+    return this._sendRequest(http, url, 'POST', params, opt)
+  }
+
+  put(http, url, params, opt) {
+    return this._sendRequest(http, url, 'PUT', params, opt)
+  }
+
+  option(http, url, params, opt) {
+    return this._sendRequest(http, url, 'OPTION', params, opt)
+  }
+
+  delete(http, url, params, opt) {
+    return this._sendRequest(http, url, 'DELETE', params, opt)
+  }
+
+}
+
+function VFetch(option, http = fetch) {
+  const { allow } = option
+  const clientWrapper = new HttpShell(option)
+  const client = {
+    injectAfter: clientWrapper.injectAfter.bind(clientWrapper),
+    injectBefore: clientWrapper.injectBefore.bind(clientWrapper),
+    setErrorHook: clientWrapper.setError.bind(clientWrapper),
+  }
+  const allowMethod = allow || ['get', 'post', 'put', 'delete', 'option']
+
+  allowMethod.forEach(m => {
+    client[m] = async (url, params, opt) => clientWrapper[m](http, url, params, opt)
+  })
+
+  return client
+
+}
+
+const httpConfig = {
+  conf: {
+    credentials: 'include',
+    baseUrl: '/api',
+    headers: {
+      Accept: 'application/json',
+      // 'Content-Type': 'application/json',
+    },
+  },
+  before: [
+    ([url, opt]) => {
+      console.log('hook1', url, opt)
+    },
+    ([url, opt]) => {
+      console.log('hook2', url, opt)
+    },
+  ],
+  after: [
+    (rsp) => {
+      console.log('after hook1', rsp)
+    },
+  ],
+  timeout: 5000,
+}
+
+
+VFetch.HttpError = HttpError
+VFetch.HTTP_ERROR_MAP = HTTP_ERROR_MAP
+VFetch.httpConfig = httpConfig
+
+if (typeof exports === 'object' && typeof module === 'object') {
+  module.exports = VFetch
+  module.exports.default = VFetch
+} else {
+  window.VFetch = VFetch
+}
